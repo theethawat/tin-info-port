a130abb87f84b3fc3d5a6fc0cbd62cabc8eb5f15
Theethawat Savastham <5910110150@psu.ac.th>
2020-05-19 01:05:55 +0700

    add list personal data

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,56 @@
 import Router from "next/router"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-export default function About() {
+function About({ data }) {
   return (
     <div>
       <section className="section">
         <div className="container">
           <Header />
+          <h3 className="title is-4">Personal Info and Biography</h3>
+          <div className="columns">
+            <div className="column is-3">
+              <img
+                className="img-fluid tin-profile-img"
+                alt="Personal Profile Image"
+                src="/Me.jpg"
+              />
+            </div>
+            <div className="column is-6">
+              <div className="content">
+                <ul>
+                  <li>
+                    Name - {data.name} {data.surname} ({data.nickname}){" "}
+                  </li>
+                  <li>Hometown - {data.hometowm}, Thailand </li>
+                  <li>Birthday - {data.birthday} </li>
+                  <li>
+                    Schools
+                    <ul>
+                      <li>{data.school[0]}</li>
+                      <li> {data.school[1]}</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <Footer />
     </div>
   )
 }
+export async function getStaticProps() {
+  const res = await fetch(
+    "http://cloud.theduckcreator.in.th/api/personal/personal-data/"
+  )
+  const data = await res.json()
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default About
