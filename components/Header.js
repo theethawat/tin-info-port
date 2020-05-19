43fef879af3840270d0e6053f5738d57e290d79d
Theethawat Savastham <5910110150@psu.ac.th>
2020-05-19 14:18:00 +0700

    Add skill and Heading

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,21 @@
 import Link from "next/link"
+import Head from "next/head"
 const Header = () => (
   <div>
+    <Head>
+      <title>TheethawatSpace*</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta http-equiv="Content–Type" content="text/html ; charset= utf–8" />
+      <meta
+        name="description"
+        content="TheethawatSpace is a Development Space of Theethawat Savastham, Theethawat's Development Portfolio Go Here!"
+      />
+      <meta name="author" content="Theethawat Savastham" />
+      <meta
+        name="keyword"
+        content="Theethawat Savastham,ธีร์ธวัช สวาสดิ์ธรรม"
+      />
+    </Head>
     <h1 className="title is-2 ">TheethawatSpace*</h1>
 
     <nav className="breadcrumb kanitlight" aria-label="breadcrumbs">
--- pages/skill.js
+++ pages/skill.js
@@ -1,9 +1,53 @@
 import Router from "next/router"
 import Header from "../components/Header"
-export default function Skill() {
+import Footer from "../components/Footer"
+function Skill({ data }) {
   return (
-    <div className="container">
-      <Header />
+    <div>
+      <section className="section">
+        <div className="container">
+          <Header />
+          <h3 className="title is-3">Skill</h3>
+          <div className="content column is-7">
+            <ul>
+              {data.map((skill) => (
+                <li>
+                  {" "}
+                  {skill.name} <br />
+                  <progress
+                    className="progress is-primary"
+                    value={skill.skillPercent}
+                    max="100"
+                  >
+                    {" "}
+                    {skill.skillPercent} %{" "}
+                  </progress>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <h3 className="title is-3">Online Badge</h3>
+          <p>Nothing to show now.</p>
+          <br />
+          <h3 className="title is-3">Certificate</h3>
+          <p>Nothing to show now.</p>
+        </div>
+      </section>
+      <Footer />
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch(
+    "http://cloud.theduckcreator.in.th/api/personal/skill/"
+  )
+  const data = await res.json()
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+export default Skill
